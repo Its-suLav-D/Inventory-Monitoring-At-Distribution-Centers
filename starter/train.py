2ccb99d86df372754587a64fc7c8af83f18b8e82
Sulove <dah19001@byui.edu>
2023-07-10 19:41:34 +0000

    update-capstone

--- starter/train.py
+++ starter/train.py
@@ -1,79 +1,78 @@
-#TODO: Import your dependencies.
-#For instance, below are some dependencies you might need if you are using Pytorch
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import torchvision.models as models
-import torchvision.transforms as transforms
-
+import os
 import argparse
+import logging
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.applications import EfficientNetB7
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.metrics import SparseCategoricalAccuracy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+logging.basicConfig(level=logging.INFO)
+
+def train(model, train_loader, epochs):
+    model.fit(train_loader, epochs=epochs, verbose=1)
+
 
 def test(model, test_loader):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+    loss, accuracy = model.evaluate(test_loader, verbose=1)
+    logging.info(f'Test accuracy: {accuracy}')
+    return accuracy
 
-def train(model, train_loader, criterion, optimizer):
-    '''
-    TODO: Complete this function that can take a model and
-          data loaders for training and will get train the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
-    
-def net():
-    '''
-    TODO: Complete this function that initializes your model
-          Remember to use a pretrained model
-    '''
-    pass
+def create_model():
+    base_model = EfficientNetB7(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+    base_model.trainable = False
+    model = Sequential([
+        base_model,
+        Flatten(),
+        Dense(5, activation='softmax')
+    ])
+    return model
 
-def create_data_loaders(data, batch_size):
-    '''
-    This is an optional function that you may or may not need to implement
-    depending on whether you need to use data loaders or not
-    '''
-    pass
+def create_data_loaders(data_dir, batch_size):
+    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+    train_generator = train_datagen.flow_from_directory(
+        data_dir,
+        target_size=(224, 224),
+        batch_size=batch_size,
+        class_mode='sparse',
+        subset='training'
+    )
+    validation_generator = train_datagen.flow_from_directory(
+        data_dir,
+        target_size=(224, 224),
+        batch_size=batch_size,
+        class_mode='sparse',
+        subset='validation'
+    )
+    return train_generator, validation_generator
 
 def main(args):
-    '''
-    TODO: Initialize a model by calling the net function
-    '''
-    model=net()
-    
-    '''
-    TODO: Create your loss and optimizer
-    '''
-    loss_criterion = None
-    optimizer = None
-    
-    '''
-    TODO: Call the train function to start training your model
-    Remember that you will need to set up a way to get training data from S3
-    '''
-    model=train(model, train_loader, loss_criterion, optimizer)
-    
-    '''
-    TODO: Test the model to see its accuracy
-    '''
-    test(model, test_loader, criterion)
-    
-    '''
-    TODO: Save the trained model
-    '''
-    torch.save(model, path)
+    model = create_model()
+    model.compile(optimizer=Adam(learning_rate=args.learning_rate),
+                  loss=SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=[SparseCategoricalAccuracy()])
+
+    train_loader, test_loader = create_data_loaders(args.data_dir, args.batch_size)
+    train(model, train_loader, args.epochs)
+    accuracy = test(model, test_loader)
+    model.save(os.path.join('/opt/ml/model', '1'), save_format='tf')
+
+
+
 
 if __name__=='__main__':
-    parser=argparse.ArgumentParser()
-    '''
-    TODO: Specify all the hyperparameters you need to use to train your model.
-    '''
-    
-    args=parser.parse_args()
-    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--epochs', type=int, default=3)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data_dir', type=str, default=os.environ["SM_CHANNEL_TRAIN"],
+)
+
+    args = parser.parse_args()
+
     main(args)
